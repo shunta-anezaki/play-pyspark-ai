2de26a88a3cc466a473f6bd0773c0665a883788d
shunta-anezaki <shunta.anezaki@magia.co.jp>
2023-07-04 16:33:20 +0900

    it works

--- src/main.py
+++ src/main.py
@@ -10,6 +10,6 @@ spark_ai = SparkAI(llm=llm,verbose=True)
 spark_ai.activate()
 
 auto_df = spark_ai.create_df("https://www.carpro.com/blog/full-year-2022-national-auto-sales-by-brand")
+spark_ai.commit()
 
-auto_top_growth_df = auto_df.ai.transform("成長したブランドは？")
-auto_top_growth_df.show()
+auto_df.ai.plot("米国の販売市場シェアの円グラフ。上位 5 ブランドとその他のブランドの合計を表示します")  
